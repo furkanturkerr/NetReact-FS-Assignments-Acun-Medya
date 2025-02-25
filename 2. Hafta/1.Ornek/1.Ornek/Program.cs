1d765d7a8105a33f93b34ed7e9f3d8285e0157d1
Furkan Türker <furkanturker.dev@gmail.com>
2025-02-25 19:23:14 +0300

    2.Hafta

---
+++ 2. Hafta/1.Ornek/1.Ornek/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1 - For döngüsü ile sayının rakamlarının toplamını bulan algoritmayı yazınız.
+
+        Console.Write("Sayıyı Giriniz: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int toplam = 0;
+        for(; sayi > 0; sayi /= 10)
+        {
+            toplam += sayi % 10;
+        }
+
+        Console.WriteLine("Sayının Rakamları Toplamı: " + toplam);
+    }
+}
---
+++ 2. Hafta/2.Ornek/2.Ornek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //2 -  while döngüsü ile kullanıcıdan belirli şartlara uyan bir değer alan algoritmayı yazınız(Kullanıcıdan 10 ile 100 arasında bir sayı al ve eğer bu 
+        //    aralık dışında girerse tekrar iste.)
+
+        Console.Write("10 ile 100 arasında bir sayı gir; ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        while(sayi < 10 || sayi > 100){
+            Console.WriteLine("10 ile 100 arasında bir sayı girmediniz tekrar giriniz; ");
+            sayi = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Sayınız: " + sayi);
+    }
+}
