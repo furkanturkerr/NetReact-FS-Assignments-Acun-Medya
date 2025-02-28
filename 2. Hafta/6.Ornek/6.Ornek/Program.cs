a76f642bb3b52ebf9b96e17ccf0e4866cec945a1
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-01 02:00:56 +0300

    Ornek-6

---
+++ 2. Hafta/6.Ornek/6.Ornek/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //6 - Kullanıcının girdiği bir cümleyi diziye kaydeden ve alfabetik olarak sıralayan algoritmayı yazınız.
+
+        string[] cumle = new string[4];
+        Array.Sort(cumle);
+
+
+        Console.Write("4 adet kelime gir: ");
+        for(int i=0; i < cumle.Length; i++)
+        {
+            cumle[i] = Console.ReadLine();
+        }
+
+        foreach(string sirala in cumle)
+        {
+            Console.WriteLine(sirala);
+        }
+
+
+    }
+}
