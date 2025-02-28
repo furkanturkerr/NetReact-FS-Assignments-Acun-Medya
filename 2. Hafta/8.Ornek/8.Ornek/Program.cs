7b0d5e54378f12f4edf3991691f02618a2c2cf2c
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-01 02:31:50 +0300

    8.Örnek

---
+++ 2. Hafta/8.Ornek/8.Ornek/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //8 - Kullanıcının girdiği kelimeleri listeye kaydeden ve tersten yazdıran algoritmayı yazınız.
+
+        Console.Write("5 tane kelime Gir: ");
+
+        List<string> kelime = new List<string>();
+        for(int i=0; i < 5; i++)
+        {
+            string a = Console.ReadLine();
+            kelime.Add(a);
+        }
+
+        kelime.Reverse(); //tersten sıralıyormuş
+
+        Console.WriteLine("Kelimeler tersten sıralandı:");
+        foreach (string kelimeler in kelime)
+        {
+            Console.WriteLine(kelimeler);
+        }
+
+
+    }
+}
