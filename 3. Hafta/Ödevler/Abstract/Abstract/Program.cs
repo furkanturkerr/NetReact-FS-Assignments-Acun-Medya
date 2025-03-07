60e8c04a92c736bc708304f9a0d37ed8f816fb6b
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-07 21:22:48 +0300

    3.hafta

---
+++ 3. Hafta/Ödevler/Abstract/Abstract/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Database database = new Oracle();
+        database.Delete();
+        database.add();
+
+        Database database2 = new Oracle();
+        database.Delete();
+        database.add();
+
+        Console.ReadLine();
+    }
+
+    abstract class Database
+    {
+        public void add()
+        {
+            Console.WriteLine("Added by default");
+        }
+
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Sql");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+
+
+
+}
---
+++ 3. Hafta/Ödevler/Interface/Interface/Program.cs
@@ -0,0 +1,60 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PersonManager personManager = new PersonManager();
+        Customer customer = new Customer
+        {
+            id = 1,
+            FirstName = "furkan",
+            LastName = "türker",
+            adress = "Samsun"
+        };
+
+        Student student = new Student
+        {
+            id = 1,
+            FirstName = "Ali",
+            LastName = "veli",
+            Departmant = "Computer"
+        };
+
+        personManager.Add(student);
+        personManager.Add(customer);
+
+        Console.ReadLine();
+    }
+
+    interface IPerson
+    {
+        int id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string adress { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
