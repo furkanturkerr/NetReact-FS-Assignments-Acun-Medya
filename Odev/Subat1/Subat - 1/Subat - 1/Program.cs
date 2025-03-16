e0033592385dfc3f03d55e3e351f7d4df9208c80
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-16 13:50:30 +0300

    Homework

---
+++ Odev/Subat1/Subat - 1/Subat - 1/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //5 elemanlı bir int dizisi oluşturup, kullanıcıdan aldığı değerlerle diziyi dolduran ve ekrana yazdıran bir program yazın.
+        //Bir dizinin içindeki en büyük sayıyı bulan bir C# programı yazın.
+
+        int[] dizi = new int[5];
+
+        Console.WriteLine("5 adet sayı gir: ");
+        for (int i=0; i<dizi.Length; i++)
+        {
+            dizi[i] = int.Parse(Console.ReadLine());
+        }
+
+        foreach(int sayılar in dizi)
+        {
+            Console.WriteLine(sayılar);
+        }
+
+        int max = dizi[0];
+
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            if (dizi[i] > max)
+            {
+                max = dizi[i];
+            }
+        }
+
+        Console.WriteLine($"En büyük sayı: {max}");
+
+    }
+}
