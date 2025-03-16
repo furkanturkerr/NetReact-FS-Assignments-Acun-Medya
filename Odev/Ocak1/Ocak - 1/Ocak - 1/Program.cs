50e0b1189c40bea4f823acf18ce0e46f1b2a1d45
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-16 12:49:25 +0300

    Homework

---
+++ Odev/Ocak1/Ocak - 1/Ocak - 1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Kullanıcıdan adını alıp ekrana yazdıran basit bir C# programı yazın.
+
+        Console.Write("Adını yaz: ");
+        string ad = Console.ReadLine();
+
+        Console.WriteLine(ad);
+    }
+}   
