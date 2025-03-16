8a9691272d8ab4a1c7980ffaa44fe337cfe2b191
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-16 13:23:18 +0300

    Homework

---
+++ Odev/Ocak4/Ocak - 4/Ocak - 4/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //1’den 10’a kadar olan sayıları ekrana yazdıran bir for döngüsü yazın.
+
+        for(int i=1; i<=10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        //Kullanıcıdan alınan bir sayıya kadar olan sayıların toplamını hesaplayan bir program yazın.
+
+        Console.Write("Bir Sayı gir: ");
+        int s1 = int.Parse(Console.ReadLine());
+
+        int toplam = 0;
+
+        for(int i=1; i< s1; i++)
+        {
+            toplam += i;  
+        }
+
+        Console.WriteLine(toplam);
+    }
+}
