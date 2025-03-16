863cc05c2301ebae74792d33a82b1b8638f655c3
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-16 13:14:23 +0300

    Homework

---
+++ Odev/Ocak3/Ocak - 3/Ocak - 3/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Kullanıcıdan bir sayı alıp tek mi çift mi olduğunu bulan bir C# programı yazın.
+
+        Console.Write("Sayı gir: ");
+        int s = int.Parse(Console.ReadLine());
+
+        if (s % 2 != 0)
+        {
+            Console.WriteLine("Sayı Tek");
+        }
+        else
+        {
+            Console.WriteLine("Sayı Çift");
+        }
+
+        //Girilen bir sayının pozitif, negatif veya sıfır olup olmadığını belirleyen bir program yazın.
+
+        Console.Write("Sayı gir: ");
+        int s1 = int.Parse(Console.ReadLine());
+
+        if (s1 > 0){
+            Console.WriteLine("Sayı Pozitif");
+        }
+        else  if(s1 < 0)
+        {
+            Console.WriteLine("Sayı Negaif");
+        }
+        else
+        {
+            Console.WriteLine("Sayı 0 a eşit");
+        }
+    }
+}
