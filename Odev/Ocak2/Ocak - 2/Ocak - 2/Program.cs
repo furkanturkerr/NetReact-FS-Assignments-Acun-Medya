7a92489bbedbf4b4b8525eb3b37464f8910f9783
Furkan Türker <furkanturker.dev@gmail.com>
2025-03-16 13:02:17 +0300

    Homework

---
+++ Odev/Ocak2/Ocak - 2/Ocak - 2/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Kullanıcıdan iki sayı alıp toplamını ekrana yazdıran bir program yazın.
+
+        Console.Write("1. Sayıyı Gir: ");
+        int s1 = int.Parse(Console.ReadLine());
+
+        Console.Write("2. Sayıyı Gir: ");
+        int s2 = int.Parse(Console.ReadLine());
+
+        int t = s1 + s2;
+        Console.Write($"Toplam: {t}");
+    }
+}
